e5484dd2342b07e71ac989f14e0ecf02962ecd16
Bernadette Davis <bsd231@nyu.edu>
2019-10-29 21:11:45 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find(params[:username])
+    @user = Helpers.current_user(session)
     #if @user
       session[:user_id] = @user.id
       redirect to '/account'
