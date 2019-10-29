d892fc065ee99dcc27ae0831c97213895610db2a
Bernadette Davis <bsd231@nyu.edu>
2019-10-29 21:41:36 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.where("username == ?", params[:username])
+    @user = User.find_by(username: params["username"], password: params["password"])
+    #@user = User.where("username == ?", params[:username])
     if @user
       #session[:user_id] = @user[:id].to_s
     #@user = Helpers.current_user(session)
