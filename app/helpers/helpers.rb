dbcd9110e11215293a92098e0c445fda1a17a8e1
Bernadette Davis <bsd231@nyu.edu>
2019-10-29 21:09:37 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -3,6 +3,11 @@ class Helpers
     User.find(session[:user_id])
   end
 
-  def is_logged_in?
+  def is_logged_in?(session)
+    if session[:user_id] != nil
+      return true
+    else
+      return false
+    end
   end
 end
