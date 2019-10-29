ad7efb9b9c8c4986eaf6d198621ecc1f35046c2e
Bernadette Davis <bsd231@nyu.edu>
2019-10-29 21:21:07 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    session[:user_id] = Helpers.current_user(session).id
+    #session[:user_id] = Helpers.current_user(session).id
     #@user = Helpers.current_user(session)
     #if @user
 
